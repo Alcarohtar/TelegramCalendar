ed0fb4df35ff8c65e000754d69e92c02081ae293
Alcarohtar <luigirocco86@gmail.com>
2022-12-13 12:53:20 +0100

    private info modified. New chat_id

--- Calendario_privateInfo.py
+++ Calendario_privateInfo.py
@@ -1,10 +1,10 @@
 
-TOKEN="5608763555:AAFUjUcrwX93yrWiNk6ytz-XoqtYQzAfXeg"
-user="famiglia_rocco"
-password="nero1986"
-host="localhost"
+TOKEN=""
+user=""
+password=""
+host=""
 port=3306
-database="calendario"
-user1=("Luigi","Rocco")
-user2=("Maria","Izzo")
-chat_id="1862184763"
+database=""
+user1=("","")
+user2=("","")
+chat_id=""
